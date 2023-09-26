1dfbdb6af38da67dc84ce6e8782990c32261b144
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2023-09-26 19:12:36 +0900

    현상황

--- app/src/main/java/com/pinekim/android_qr_scannver/MainActivity.kt
+++ app/src/main/java/com/pinekim/android_qr_scannver/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.journeyapps.barcodescanner.ScanContract
@@ -18,8 +19,7 @@ import com.pinekim.android_qr_scannver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var mBinding:ActivityMainBinding? = null
-    private val binding get() =mBinding!!
+    private lateinit var mBinding : ActivityMainBinding
 
     private var btnQrScanner: Button? = null
     val lottoModel = LottoModel()
@@ -31,32 +31,32 @@ class MainActivity : AppCompatActivity() {
         ScanContract()
     ) { result: ScanIntentResult ->
         // result : 스캔된 결과
-
-        // 내용이 없다면
-        if (result.contents == null) {
+        if (result.contents == null) {// 내용이 없다면
             Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
-        } else {
-            // 내용이 있다면
-
+        } else { // 내용이 있다면
             // Toast 메세지 출력
             /*Toast.makeText(this, "Scanned : " + result.contents, Toast.LENGTH_SHORT).show()*/
-            val lottoResult = result.contents.substring(28)
+            val lottoResult = result.contents.split("=")
 
-
-            /*val times = lottoResult.substring(0, 4) // 로또 회차*/
-            lottoModel.tiems = lottoResult.substring(0, 4) // 로또 회차
-            lottoModel.lottoLineNum1 = lottoResult.substring(5, 17) // 첫번째
-            lottoModel.lottoLineNum2 = lottoResult.substring(18, 30) // 두번째
-            lottoModel.lottoLineNum3 =lottoResult.substring(31, 43) // 세번째
-            lottoModel.lottoLineNum4 = lottoResult.substring(44, 56) // 네번째
-            lottoModel.lottoLineNum5 = lottoResult.substring(57, 69) // 다섯번쨰
-            lottoModel.lottoNumTR = lottoResult.substring(69) // TR Number
+            lottoModel.tiems = lottoResult[1].substring(0, 4) // 로또 회차
+            val number = lottoResult[1].split("p")
+            for (i in 0..number.size) {
+                Log.e("gyoungTae", number[i])
+            }
+            /*lottoModel.lottoLineNum1 = lottoResult[1].substring(5, 17) // 첫번째 라인 xx, xx, xx, xx, xx, xx
+            lottoModel.lottoLineNum2 = lottoResult[1].substring(18, 30) // 두번째 라인 xx, xx, xx, xx, xx, xx
+            lottoModel.lottoLineNum3 =lottoResult[1].substring(31, 43) // 세번째 라인 xx, xx, xx, xx, xx, xx
+            lottoModel.lottoLineNum4 = lottoResult[1].substring(44, 56) // 네번째 라인 xx, xx, xx, xx, xx, xx
+            lottoModel.lottoLineNum5 = lottoResult[1].substring(57, 69) // 다섯번쨰 라인 xx, xx, xx, xx, xx, xx
+            lottoModel.lottoNumTR = lottoResult[1].substring(69) // TR Number*/
 
            /* for(i: Int in ) {
 
             }*/
 
-            Toast.makeText(this, lottoResult, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, lottoResult.toString(), Toast.LENGTH_SHORT).show()
+            /*Log.e("gyoungTae", result.contents)
+            Log.e("gyoungTae", lottoResult.toString())
             Log.e("gyoungTae", lottoModel.tiems.toString())
             Log.e("gyoungTae", lottoModel.lottoLineNum1.toString())
             Log.e("gyoungTae", lottoModel.lottoLineNum2.toString())
@@ -65,7 +65,7 @@ class MainActivity : AppCompatActivity() {
             Log.e("gyoungTae", lottoModel.lottoLineNum5.toString())
             Log.e("gyoungTae", lottoModel.lottoNumTR.toString())
             // 1번 라인 번호 따로 저장
-            Log.e("gyoungTae", lottoModel.lottoLineNum1!!.substring(0, 2))
+            Log.e("gyoungTae", lottoModel.lottoLineNum1!!.substring(0, 2))*/
 
             // 결과 값 로그 출력
             /*Log.e("gyoungTae", result.contents.toString())*/
@@ -81,10 +81,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        btnQrScanner = binding.btnQrScan
+        btnQrScanner = mBinding.btnQrScan
 
         initQrScanner()
         initRecyclerViewSetting()
@@ -98,7 +97,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun initRecyclerViewSetting() {
-        val rv: RecyclerView = binding.lottoNumRv
+        val rv: RecyclerView = mBinding.lottoNumRv
 
         rvAdapte = LottoRVAdapter(this)
         rv.adapter = rvAdapte
